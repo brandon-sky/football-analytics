571163060f69fa04cda421df6423698d1c04df2c
bgs <brandon.sai@ipa.fraunhofer.de>
2023-09-22 00:02:48 +0200

    feat: extract all boxscore links from page

--- src/main.py
+++ src/main.py
@@ -11,19 +11,30 @@ URL ="https://www.pro-football-reference.com"
 BASE_URL = URL+"/boxscores"
 
 
-
-
-def extract_current_week_info(soup: BeautifulSoup) -> Tuple[str]:
+def extract_current_week_info(soup: BeautifulSoup) -> str:
     h2_element = soup.select_one('h2').text
-    return h2_element.split()
+    return h2_element
 
+def extract_all_boxscore_links(soup: BeautifulSoup) -> list:
+    final_links = soup.select('a:-soup-contains("Final")')
+    return [BASE_URL+link["href"] for link in final_links]
 
 def main():
+    # Request from site
     http_session = Session()
     html = request_url(session=http_session, url=BASE_URL)
     soup = parse_html_to_soup(html=html)
+
+    # Extract
     last_gameday = extract_current_week_info(soup=soup)
-    print(f"{last_gameday = }")
+    boxscore_links = extract_all_boxscore_links(soup=soup)
+
+    # Load
+    links_to_crawl = {
+        last_gameday: boxscore_links
+    }
+
+    print(links_to_crawl)
     return
 
 if __name__ == "__main__":
