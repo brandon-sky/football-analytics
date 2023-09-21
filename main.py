815cc5263d0ca9c5993e394859a5abba560b3499
bgs <brandon.sai@ipa.fraunhofer.de>
2023-09-21 15:49:45 +0200

    feat: base url defined

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-BASE_URL ="https://www.pro-football-reference.com"
+URL ="https://www.pro-football-reference.com"
+BASE_URL = URL+"/boxscores"
+
+def main():
+    print(BASE_URL)
+    return
+
+if __name__ == "__main__":
+    main()
